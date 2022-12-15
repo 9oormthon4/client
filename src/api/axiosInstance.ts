9a9244cb4cf766aaa900966a659a8cf10f56d60e
ryuna <anottrx@gmail.com>
2022-12-15 22:19:19 +0900

    fix: header 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -4,9 +4,11 @@ const axiosConfig: AxiosRequestConfig = {
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
-    'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Origin': '*',
     'Accept': 'application/json',
-    "Access-Control-Allow-Credentials": true,
+    // "Access-Control-Allow-Credentials": 'true',
+    // 'Access-Control-Max-Age': 86400,
+    // 'Access-Control-Allow-Methods': 'GET, POST, PATCH',
     "id": "jeju",
     "password": "pwd"
   },
