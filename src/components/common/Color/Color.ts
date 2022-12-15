130808e92d7b340861feadcc6221ec4cc4c4c54f
ryuna <anottrx@gmail.com>
2022-12-15 11:54:37 +0900

    refactor: 코드 리팩터링 (#12)

--- src/components/common/Color/Color.ts
+++ src/components/common/Color/Color.ts
@@ -1,5 +1,12 @@
 const Color = {
-  mainTheme: '#2F8872'
+  mainTheme: '#2F8872',
+  orange: '#FF971E',
+  green: '#0E8821',
+  sub: '#515151',
+  black: '#2B2B2B',
+  white: '#FFFFFF',
+  orange2: '#F27400',
+  green2: '#356914'
 }
 
 export default Color
---
+++ src/types/response.ts
@@ -0,0 +1,5 @@
+export interface ResponsesType {
+  "userId": number
+  "questionId": number
+  "answerToQuestion": number
+}
