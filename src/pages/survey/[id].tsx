b74f0d8c25f544498ea9f5030a8035d1d8eedbba
ryuna <anottrx@gmail.com>
2022-12-15 11:52:53 +0900

    feat: 설문 답변 스타일링 (#12)

--- src/pages/survey/[id].tsx
+++ src/pages/survey/[id].tsx
@@ -3,33 +3,100 @@ import { useEffect } from 'react';
 
 import { Layout } from '@/components/common/Layout';
 import ProgressBar from '@/components/common/ProgressBar';
-import Button from '@/components/common/Button';
+import ButtonAnswer from '@/components/common/ButtonAnswer';
 import styled from '@emotion/styled';
 import { useSelector } from '@/store';
+import { useDispatch } from 'react-redux';
+import { answerActions } from '@/store/answer';
+import { useRouter } from 'next/router';
+import { Container } from '@/components/common/Layout/Layout';
+import Color from '@/components/common/Color';
 
 type SurveyDetailProps = {
   id: number;
 };
 
 const SurveyDetail: NextPage<SurveyDetailProps> = ({ id }) => {
+  const router = useRouter();
+  
   const data = useSelector((state) => state.questions);
 
-  console.log(data)
+  const requestId = useSelector((state) => state.requestId);
+
+  const totalQuestionCount = useSelector((state) => state.questions.totalQuestionCount)
+
+  const dispatch = useDispatch();
+
+  const answers = useSelector((state) => state.answer.responses);
+
+  // console.log(id, data.questions[id])
   
   useEffect(() => {
     document.title = '어드레감디 | 설문';
   }, []);
 
+  const moveRouter = () => {
+    if(id < totalQuestionCount) {
+      router.push(`/survey/${Number(id) + 1}`);
+    } else {
+      // POST
+      router.push(`/result`);
+    }
+  }
+
   return (
     <Layout>
-      <ProgressBar id={id} />
-      <Question>{data.questions[id]?.questionContent}</Question>
-      <TopMargin>
-      </TopMargin>
-        <ButtonWrapper><Button onClick={() => console.log()}>{data.questions[id]?.optionOne}</Button></ButtonWrapper>
-      <ButtonWrapper><Button onClick={() => console.log()}>{data.questions[id]?.optionTwo}</Button></ButtonWrapper>
-      <ButtonWrapper><Button onClick={() => console.log()}>{data.questions[id]?.optionThree}</Button></ButtonWrapper>
-      <Button onClick={() => console.log()}>{data.questions[id]?.optionFour}</Button>
+      <Container>
+        <Wrapper>
+          <ProgressBar id={id} total={totalQuestionCount} />
+          <QuestionType>Q{id}. {data.questions[id-1]?.questionContent}</QuestionType>
+          <TopMargin>
+          </TopMargin>
+          <ButtonWrapper>
+            <ButtonAnswer backgroundColor={Color.green} fontSize={'23px'} id={1} onClick={() => {
+            const curAnswerList = [...answers];
+            // curAnswerList[curAnswerList.length] = {
+            //   "userId": requestId.userId,
+            //   "questionId": id,
+            //   "answerToQuestion": 1
+            // };
+            // dispatch(answerActions.setAnswer({responses: curAnswerList}));
+            moveRouter();
+          } }>{data.questions[id-1]?.optionOne}</ButtonAnswer></ButtonWrapper>
+          <ButtonWrapper>
+            <ButtonAnswer backgroundColor={Color.green} fontSize={'23px'} id={2} onClick={() => {
+            const curAnswerList = [...answers];
+            // curAnswerList[curAnswerList.length] = {
+            //   "userId": requestId.userId,
+            //   "questionId": id,
+            //   "answerToQuestion": 2
+            // };
+            // dispatch(answerActions.setAnswer({responses: curAnswerList}));
+            moveRouter();
+          }}>{data.questions[id-1]?.optionTwo}</ButtonAnswer></ButtonWrapper>
+          <ButtonWrapper>
+            <ButtonAnswer backgroundColor={Color.green} fontSize={'23px'} id={3} onClick={() => {
+            const curAnswerList = [...answers];
+            // curAnswerList[curAnswerList.length] = {
+            //   "userId": requestId.userId,
+            //   "questionId": id,
+            //   "answerToQuestion": 3
+            // };
+            // dispatch(answerActions.setAnswer({responses: curAnswerList}));
+            moveRouter();
+          }}>{data.questions[id-1]?.optionThree}</ButtonAnswer></ButtonWrapper>
+          <ButtonAnswer backgroundColor={Color.green} fontSize={'23px'} id={4} onClick={() => {
+            const curAnswerList = [...answers];
+            // curAnswerList[curAnswerList.length] = {
+            //   "userId": requestId.userId,
+            //   "questionId": id,
+            //   "answerToQuestion": 4
+            // };
+            // dispatch(answerActions.setAnswer({responses: curAnswerList}));
+            moveRouter();
+          }}>{data.questions[id-1]?.optionFour}</ButtonAnswer>
+        </Wrapper>
+      </Container>
     </Layout>
   );
 };
@@ -39,8 +106,19 @@ SurveyDetail.getInitialProps = async ({ query }: any) => {
   return { id };
 };
 
-const Question = styled.div`
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${Color.orange}
+`;
+
+const QuestionType = styled.div`
   margin-top: 70px;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 27.54px;
 `
 
 const TopMargin = styled.div`
