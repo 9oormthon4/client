cad43bfa13216f667372689815f5b3c0b25f1456
ryuna <anottrx@gmail.com>
2022-12-15 11:52:17 +0900

    design: 메인페이지 스타일링 (#11)

--- src/components/common/Layout/Layout.ts
+++ src/components/common/Layout/Layout.ts
@@ -4,4 +4,10 @@ export const Layout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100%;
 `;
+
+export const Container = styled.div`
+  width: 390px;
+  height: 900px;
+`
