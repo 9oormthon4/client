b600b0bd2f847879ebcd73b69db0fab60f60a1c7
ryuna <anottrx@gmail.com>
2022-12-15 14:32:23 +0900

    design: 결과페이지 배경 스타일링 (#13)

---
+++ src/components/features/Result/Result.styled.ts
@@ -0,0 +1,10 @@
+import Color from "@/components/common/Color";
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${Color.skyblue}
+`;
---
+++ src/components/features/Result/Result.tsx
@@ -0,0 +1,11 @@
+import { Container, Layout } from "@/components/common/Layout/Layout";
+import { Wrapper } from "./Result.styled";
+
+const Result = () => {
+  return <Layout>
+    <Container>
+        <Wrapper>
+    결과페이지</Wrapper></Container></Layout>
+}
+
+export default Result;
