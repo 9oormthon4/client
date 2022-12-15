c1079489613b270ac20056356fefd3103a421dbe
ryuna <anottrx@gmail.com>
2022-12-16 03:19:25 +0900

    design: 칩 스타일링 (#19)

---
+++ src/components/common/Chip/Chip.styled.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+import Color from '../Color';
+
+export const Layout = styled.div`
+  // width: 170px;
+  // height: 24px;
+  left: 110px;
+  top: 124px;
+  background: ${Color.black};
+  border-radius: 50px;
+  padding: 2px 20px;
+`;
+
+export const TextWrapper = styled.div`
+  // position: absolute;
+  // text-align: start;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  color: ${Color.white}
+`
