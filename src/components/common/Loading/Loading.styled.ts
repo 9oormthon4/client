7f2d4778bcbb24de54122910952ee886cb33230d
ryuna <anottrx@gmail.com>
2022-12-16 03:18:57 +0900

    design: 로딩 컴포넌트 (#19)

---
+++ src/components/common/Loading/Loading.styled.ts
@@ -0,0 +1,14 @@
+import Color from "@/components/common/Color";
+import styled from "@emotion/styled";
+
+export const Layout = styled.div`
+  background-color: ${Color.green};
+`
+
+export const Description = styled.div`
+  font-size: 22px;
+  font-weight: 700;
+  line-height: 28px;
+  text-align: center;
+  color: ${Color.white};
+`
