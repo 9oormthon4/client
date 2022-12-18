39f9dd7f1c741ffc9e666c05cf432c5dd60633d7
ryuna <anottrx@gmail.com>
2022-12-19 02:03:16 +0900

    refactor: 불필요한 코드 제거 (#22)

--- src/components/common/StatisticDescription/StatisticDescription.tsx
+++ src/components/common/StatisticDescription/StatisticDescription.tsx
@@ -1,18 +1,12 @@
 import { getInnerParticipantCount } from '@/api/survey';
-import useParticipantCount from '@/hooks/useParticipantCount';
 import { useEffect, useState } from 'react';
 import { Layout } from './StatisticDescription.styled';
 
 const StatisticDescription = () => {
-  // const { isLoading, data, isError, error } = useParticipantCount();
-
-  // const data = getParticipantCount();
-
   const [totalParticipantCount, setTotalParticipantCount] = useState(0);
 
   const getTotalParticipantCount = () => {
     getInnerParticipantCount().then((result) => {
-      // console.log(result.data.totalParticipantCount)
       setTotalParticipantCount(result.data.totalParticipantCount);
     });
   };
@@ -24,19 +18,13 @@ const StatisticDescription = () => {
     // }
   }, []);
 
-  // console.log(data?.data, totalParticipantCount)
-
   return (
     <Layout>
       {totalParticipantCount!==0 ? <>지금까지 {totalParticipantCount.toLocaleString()}명이 나무를 심었어요!</> : <></>}
-
       {/* {data ?
-
       // totalParticipantCount!==0 ?
       //   (<>지금까지 {totalParticipantCount}명이 나무를 심었어요!</>) :
-
     (<>지금까지 {data?.data?.totalParticipantCount}명이 나무를 심었어요!</>) : <></>} */}
-
     </Layout>
   );
 };
