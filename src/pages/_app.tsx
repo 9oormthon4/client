b786969213523b7a5c8cb22c71798aa8588ae8a3
ryuna <anottrx@gmail.com>
2022-12-19 01:59:07 +0900

    fix: redux 설정 변경 (#22)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { wrapper } from '@/store';
+import { Provider } from 'react-redux';
 
 declare global {
   interface Window {
@@ -17,11 +17,13 @@ const app = ({ Component, ...rest }: AppProps) => {
   const {store, props} = wrapper.useWrappedStore(rest);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...props.pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...props.pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
   );
 };
 
-export default wrapper.withRedux(app);
+export default app;
